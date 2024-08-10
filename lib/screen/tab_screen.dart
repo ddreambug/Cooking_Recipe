25ae99a551b0305f976e3861f92124440f4c8a72
Dest <86515936+ddreambug@users.noreply.github.com>
2024-08-10 15:54:25 +0700

    using provider to trigger function on it own target widget (without passing around)

--- lib/screen/tab_screen.dart
+++ lib/screen/tab_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:navigation_demo/data/dummy_data.dart';
 import 'package:navigation_demo/models/meal.dart';
 import 'package:navigation_demo/screen/categories.dart';
 import 'package:navigation_demo/screen/filter_screen.dart';
