68a792252043b796d9296e4cb684fd0ac8e6647b
Dest <86515936+ddreambug@users.noreply.github.com>
2024-08-14 16:46:42 +0700

    simplify filter_screen.dart because it wont need state

--- lib/screen/filter_screen.dart
+++ lib/screen/filter_screen.dart
@@ -3,17 +3,11 @@ import 'package:navigation_demo/widget/filter_property.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:navigation_demo/providers/filter_provider.dart';
 
-class FilterScreen extends ConsumerStatefulWidget {
+class FilterScreen extends ConsumerWidget {
   const FilterScreen({super.key});
-  @override
-  ConsumerState<FilterScreen> createState() {
-    return _FilterScreenState();
-  }
-}
 
-class _FilterScreenState extends ConsumerState<FilterScreen> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final defaultFilter = ref.watch(filterProvider);
     return Scaffold(
       appBar: AppBar(
